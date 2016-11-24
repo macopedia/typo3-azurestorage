f928f7164af50a292b931d5e0127aed91700c4cd
Benjamin Hirsch <mail@benjaminhirsch.net>
2016-11-24 07:49:16 +0100

    Raised compatibility up to TYPO3 8.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.1.99'
+            'typo3' => '7.6.0-8.4.99'
         ),
     ),
 );

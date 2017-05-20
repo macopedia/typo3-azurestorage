e017357a5bb058c4f577db9fa23c895e0727b9c8
Benjamin Hirsch <mail@benjaminhirsch.net>
2017-05-20 20:27:34 +0200

    Raised version number. Removed tests completly, because they aren't working anymore and need a major revamp

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Azure Storage',
     'description' => 'Microsoft Azure Blob Storage to TYPO3',
     'category' => 'be',
-    'version' => '0.1.5',
+    'version' => '0.2.0',
     'state' => 'beta',
     'clearcacheonload' => 1,
     'author' => 'Benjamin Hirsch',
     'author_email' => 'mail@benjaminhirsch.net',
     'author_company' => '',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '7.6.0-8.9.99'
-        ),
-    ),
-);
+        ],
+    ],
+];

57bbf5b8be4e4be81ce55e4f2876f4bdfe70c1cb
Benjamin Hirsch <b.hirsch@columbus-interactive.de>
2018-10-23 16:57:20 +0200

    Pushed version in the TYPO3 emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.9.99'
+            'typo3' => '7.6.0-9.9.99'
         ],
     ],
 ];

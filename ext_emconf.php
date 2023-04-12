a5bf44178bcd978336a02e6d3f35dd0a054dc25a
Tymoteusz Motylewski <t.motylewski@gmail.com>
2023-04-12 22:46:48 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Azure Storage',
     'description' => 'Microsoft Azure Blob Storage for TYPO3',
     'category' => 'be',
-    'version' => '0.5.3',
+    'version' => '0.6.1',
     'state' => 'beta',
     'clearcacheonload' => 1,
-    'author' => 'Benjamin Hirsch',
-    'author_email' => 'mail@benjaminhirsch.net',
+    'author' => 'Macopedia.com team',
+    'author_email' => 'extensions@macopedia.pl',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-10.4.99'
+            'typo3' => '7.6.0-12.4.99'
         ],
     ],
 ];

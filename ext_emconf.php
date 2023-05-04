dcda34ff6620850304e8101792b0dd73c80ffb4f
Tymoteusz Motylewski <t.motylewski@gmail.com>
2023-05-04 23:46:54 +0200

    Remove usage TYPO3_MODE, rise TYPO3 req to 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Azure Storage',
     'description' => 'Microsoft Azure Blob Storage for TYPO3',
     'category' => 'be',
-    'version' => '0.6.1',
-    'state' => 'beta',
+    'version' => '1.0.0',
+    'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Macopedia.com team',
     'author_email' => 'extensions@macopedia.pl',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-12.4.99'
+            'typo3' => '11.5.0-12.4.99'
         ],
     ],
 ];

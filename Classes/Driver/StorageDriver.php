14eef2b709666fa669e13d12c74b797e269af910
Bernd Mohring <96518240+tromboman@users.noreply.github.com>
2025-02-13 22:35:21 +0100

    Mod mergeConfigurationCapabilities

--- Classes/Driver/StorageDriver.php
+++ Classes/Driver/StorageDriver.php
@@ -151,7 +151,7 @@ class StorageDriver extends AbstractHierarchicalFilesystemDriver
      * @param int $capabilities
      * @return int
      */
-    public function mergeConfigurationCapabilities($capabilities)
+    public function mergeConfigurationCapabilities(TYPO3\CMS\Core\Resource\Capabilities $capabilities): TYPO3\CMS\Core\Resource\Capabilities
     {
         $this->capabilities &= $capabilities;
 
